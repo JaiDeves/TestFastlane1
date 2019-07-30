0047923374a3c86ab2a0211d668677f22c63a197
Ranganath J <ranganath.j@vibrant-info.com>
2019-07-30 12:38:59 +0530

    testing GitHub web hook print statement

--- TestingFastlane1/ViewController.swift
+++ TestingFastlane1/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("JAI DEVES")
         print("NEw print addded")
+        print("testing jenkins webhook")
     }
 
 
