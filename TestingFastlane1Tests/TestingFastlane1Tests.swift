ab40d2868395088d3aa5c80c7d4adb8d736222cf
Ranganath J <ranganath.j@vibrant-info.com>
2019-07-30 17:20:19 +0530

    Added failable test case 1

--- TestingFastlane1Tests/TestingFastlane1Tests.swift
+++ TestingFastlane1Tests/TestingFastlane1Tests.swift
@@ -22,7 +22,7 @@ class TestingFastlane1Tests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Failable test case
-        XCTAssert(true)
+        XCTAssert(false)
     }
 
 
